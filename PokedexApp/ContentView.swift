834fb69c6f7edbf1fd4d8b251510eab015c48822
Joshua Mandelson <joshuamandelson@gmail.com>
2025-09-03 11:53:03 -0500

    Create first screen

--- PokedexApp/ContentView.swift
+++ PokedexApp/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Image("pokemon-background")
+                .resizable()
+            VStack{
+                Text("Pokedex")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .background(Color.white.opacity(0.8))
+                    .cornerRadius(20)
+                    
+                Button {
+                    
+                } label: {
+                    Text("Get Started")
+                        .foregroundColor(.black)
+                        .font(.headline)
+                }
+                .background(Color.white.opacity(1.0))
+                .cornerRadius(20)
+                
+            } //End V stack
+        } //End Z stack
     }
 }
 
