9532bf2d81197ba7ee2bef5348be8740fe32f9c7
Joshua Mandelson <joshuamandelson@gmail.com>
2025-09-03 11:26:00 -0500

    Initial Commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Josh Mandelson on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexAppTests/PokedexAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppTests.swift
+//  PokedexAppTests
+//
+//  Created by Josh Mandelson on 9/3/25.
+//
+
+import Testing
+@testable import PokedexApp
+
+struct PokedexAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
