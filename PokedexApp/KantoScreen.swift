25e195b76c5b0575d911e24604c0d51fc765a31e
Joshua Mandelson <joshuamandelson@gmail.com>
2025-09-04 06:35:01 -0500

    Add Images to assets for first 3 Pokemon

---
+++ PokedexApp/KantoScreen.swift
@@ -0,0 +1,18 @@
+//
+//  KantoScreen.swift
+//  PokedexApp
+//
+//  Created by Josh Mandelson on 9/4/25.
+//
+
+import SwiftUI
+
+struct KantoScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KantoScreen()
+}
