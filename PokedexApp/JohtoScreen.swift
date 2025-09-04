467ff54d440ff51d32953b4997db9b5437bd7525
Joshua Mandelson <joshuamandelson@gmail.com>
2025-09-04 13:36:21 -0500

    Add images, data, and view for johto.

---
+++ PokedexApp/JohtoScreen.swift
@@ -0,0 +1,18 @@
+//
+//  JohtoScreen.swift
+//  PokedexApp
+//
+//  Created by Josh Mandelson on 9/4/25.
+//
+
+import SwiftUI
+
+struct JohtoScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JohtoScreen()
+}
