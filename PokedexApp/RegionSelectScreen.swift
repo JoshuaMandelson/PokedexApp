c94b31144eadb21af854dd832a48b502e41d1093
Joshua Mandelson <joshuamandelson@gmail.com>
2025-09-03 20:03:34 -0500

    Add Second Screen

--- PokedexApp/RegionSelectScreen.swift
+++ PokedexApp/RegionSelectScreen.swift
@@ -7,37 +7,24 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RegionSelectScreen: View {
     var body: some View {
         NavigationStack {
             ZStack {
                 Image("pokemon-background")
                     .resizable()
-                VStack(spacing: 400){
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 50)
-                            .frame(width: 600, height: 200)
-                            .foregroundColor(.white)
-                        
-                        Text("Pokedex")
-                            .font(.system(size: 150))
-                            .foregroundColor(.blue)
-                            .background(Color.white)
-                            .cornerRadius(20)
-                            .fontWeight(.semibold)
-                    }
+                
+                HStack {
                     NavigationLink {
-                        RegionSelectScreen()
+                        
                     } label: {
                         Text("Get Started")
                             .font(.system(size: 50, weight: .semibold))
                             .padding(.horizontal, 36)
                             .padding(.vertical, 16)
                     }
-
+                }
                     
-                    
-                } //End V stack
             } //End Z stack
             .frame(width: 1280, height: 720)
         }//End Navigation stack
